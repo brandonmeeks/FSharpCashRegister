6cb72cefb761d59f4faf3a33f59f478e17580111
brandon.meeks <brandon.meeks@acadian.com>
2019-05-17 16:32:44 -0500

    made model mutable. completed additem and clear functionality.

--- Register.FSharp/CRMain.fs
+++ Register.FSharp/CRMain.fs
@@ -15,51 +15,57 @@ type MainModel = {
     Subtotal: decimal
     Tax: decimal
     Total: decimal
-}       
+}
+with
+    static member Default = {
+    ShoppingCart = []
+    Subtotal = 0m
+    Tax = 0m
+    Total = 0m
+    }
 
 type CRMainController(ui: CashRegisterMain) =
-    let initModel = {ShoppingCart = [];
-                     Subtotal = 0m;
-                     Tax = 0m;
-                     Total = 0m}
+    let mutable model = {
+        ShoppingCart = []
+        Subtotal = 0m
+        Tax = 0m
+        Total = 0m
+        }
     
-    let UpdateForm model =
+    let UpdateForm newModel =
         ui.ShoppingCart.Items.Clear()
+        model <- newModel
         model.ShoppingCart |> List.map(fun item -> ui.ShoppingCart.Items.Add(item))
-        ui.Subtotal.Text = model.Subtotal.ToString()
-        ui.Tax.Text = model.Tax.ToString()
-        ui.Total.Text = model.Total.ToString()
+        ui.Subtotal.Text <- model.Subtotal.ToString()
+        ui.Tax.Text <- model.Tax.ToString()
+        ui.Total.Text <- model.Total.ToString()
+        ui.ItemName.Text <- ""
+        ui.Price.Text <- ""
+        ui.Quantity.Text <- ""
+
+    let CalculateValues model =
+        let subtotal = model.ShoppingCart |> List.sumBy(fun p -> p.Price * (decimal <| p.Quantity))
+        let tax = subtotal * 0.08m
+        let total = subtotal + tax
+        { model with Subtotal = subtotal; Tax = tax; Total = total }
 
     let AddItem model =
-        let newModel = {ShoppingCart = {Name = ui.ItemName.Text; Price = ui.Price.Text |> decimal; Quantity = ui.Quantity.Text |> int} :: model.ShoppingCart;
-                        Subtotal = 0m;
-                        Tax = 0m;
-                        Total = 0m;}
-        newModel
-        
+        let newItem = 
+            { Name = ui.ItemName.Text; Price = ui.Price.Text |> decimal; Quantity = ui.Quantity.Text |> int } 
+        { model with ShoppingCart = newItem :: model.ShoppingCart; }
     
     do ui.btnAddItems.Click.Add(fun _ ->
-        UpdateForm (AddItem initModel) |> ignore
+        AddItem model |> CalculateValues |> UpdateForm |> ignore
         )
-
 
     do ui.btnDeleteItem.Click.Add(fun _ ->
         //delete item from model and update view with new model
         ())
      
     do ui.btnClear.Click.Add(fun _ ->
-    //reset model to initModel and update view with initModel
-    ())
+        MainModel.Default |> UpdateForm
+    )
     
     do ui.btnCheckout.Click.Add(fun _ ->
     //open Checkout form
     ())
-
-
-(*
-    
-    let CheckoutClicked =
-        let form = new FSharpCashRegister.Checkout()
-        
-     
-*)
